723ea7cd552417180901d1bac6238a4594644c3f
batuhanumay <47949233+BatuhanUmay@users.noreply.github.com>
2022-03-13 16:14:05 +0300

    2

---
+++ 2-Even Fibonacci numbers.py
@@ -0,0 +1,13 @@
+def evenFiboNums():
+    a = 1
+    b = 2
+    total = 0
+    while b < 4000000:
+        if b % 2 == 0:
+            total += b
+        a, b = b, a+b
+    return total
+
+
+if __name__ == "__main__":
+    print(evenFiboNums())
