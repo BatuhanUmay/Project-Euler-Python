9432b8f314032bce3e76a6fa7a5f2079ed6244c6
batuhanumay <47949233+BatuhanUmay@users.noreply.github.com>
2022-04-27 18:17:49 +0300

    project euler

---
+++ 44-Pentagon numbers.py
@@ -0,0 +1,81 @@
+def pentagonal_numbers(n):
+    return n * (3 * n - 1) // 2
+
+
+def is_pentagonal(x):
+    """
+    n * (3n-1)
+    ---------- = x
+        2
+    3n^2 - n - 2x = 0
+    delta = 1 - 4 * 3 * -2x = 1 + 24x
+
+                1 +- (delta)^0.5
+    x1,2 =   --------------------
+                      6
+    """
+    return ((1 + (1 + 24 * x) ** 0.5) / 6).is_integer()
+
+
+for i in range(1, 5000):
+    number1 = pentagonal_numbers(i)
+    for j in range(i - 1, 0, -1):
+        number2 = pentagonal_numbers(j)
+
+        if is_pentagonal(number1 + number2) and abs(is_pentagonal(number1 - number2)):
+            print(abs(number1 - number2))
+
+##################################################################
+
+# import itertools
+#
+#
+# def compute():
+#     pentanum = PentagonalNumberHelper()
+#     min_d = None  # None means not found yet, positive number means found a candidate
+#     # For each upper pentagonal number index, going upward
+#     for i in itertools.count(2):
+#         pent_i = pentanum.term(i)
+#         # If the next number down is at least as big as a found difference, then conclude searching
+#         if min_d is not None and pent_i - pentanum.term(i - 1) >= min_d:
+#             break
+#
+#         # For each lower pentagonal number index, going downward
+#         for j in range(i - 1, 0, -1):
+#             pent_j = pentanum.term(j)
+#             diff = pent_i - pent_j
+#             # If the difference is at least as big as a found difference, then stop testing lower pentagonal numbers
+#             if min_d is not None and diff >= min_d:
+#                 break
+#             elif pentanum.is_term(pent_i + pent_j) and pentanum.is_term(diff):
+#                 min_d = diff  # Found a smaller difference
+#     return str(min_d)
+#
+#
+# # Provides memoization for generating and testing pentagonal numbers.
+# class PentagonalNumberHelper:
+#     def __init__(self):
+#         self.term_list = [0]
+#         self.term_set = set()
+#
+#     def term(self, x):
+#         assert x > 0
+#         while len(self.term_list) <= x:
+#             n = len(self.term_list)
+#             term = (n * (n * 3 - 1)) >> 1
+#             self.term_list.append(term)
+#             self.term_set.add(term)
+#         return self.term_list[x]
+#
+#     def is_term(self, y):
+#         assert y > 0
+#         while self.term_list[-1] < y:
+#             n = len(self.term_list)
+#             term = (n * (n * 3 - 1)) >> 1
+#             self.term_list.append(term)
+#             self.term_set.add(term)
+#         return y in self.term_set
+#
+#
+# if __name__ == "__main__":
+#     print(compute())
